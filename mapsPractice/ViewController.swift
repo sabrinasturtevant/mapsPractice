a1869410cba7e417449bceb82bb318b5ead91ff9
sabrinasturtevant <sabrina.sturteva@smail.astate.edu>
2020-03-18 18:16:42 -0500

    Initial Commit

---
+++ mapsPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mapsPractice
+//
+//  Created by Sabrina Sturtevant on 3/18/20.
+//  Copyright © 2020 Sabrina Sturtevant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
